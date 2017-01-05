789505b9afc444234dde556752b4fbbeedfa92f6
Yotam Gingold <yotam@yotamgingold.com>
2017-01-05 12:11:57 -0500

    added tolerance

--- spqr.py
+++ spqr.py
@@ -111,12 +111,16 @@ def cholmod_free_triplet( A ):
     A_ptr[0] = A
     lib.cholmod_l_free_triplet( A_ptr, cc )
 
-def qr( A ):
+def qr( A, tolerance = None ):
     '''
     Given a sparse matrix A,
     returns Q, R, E, rank such that:
         Q*R = A*permutation_from_E(E)
     rank is the estimated rank of A.
+    
+    If optional `tolerance` parameter is negative, it has the following meanings:
+        #define SPQR_DEFAULT_TOL ...       /* if tol <= -2, the default tol is used */
+        #define SPQR_NO_TOL ...            /* if -2 < tol < 0, then no tol is used */
     '''
     
     chol_A = scipy2choldmod( A )
@@ -125,10 +129,12 @@ def qr( A ):
     chol_R = ffi.new("cholmod_sparse**")
     chol_E = ffi.new("SuiteSparse_long**")
     
+    if tolerance is None: tolerance = lib.SPQR_DEFAULT_TOL
+    
     rank = lib.SuiteSparseQR_C_QR(
         ## Input
         lib.SPQR_ORDERING_DEFAULT,
-        lib.SPQR_DEFAULT_TOL,
+        tolerance,
         A.shape[0],
         chol_A,
         ## Output
@@ -170,5 +176,5 @@ if __name__ == '__main__':
     # Q, R, E, rank = qr( scipy.sparse.identity(10) )
     
     M = scipy.sparse.rand( 10, 10, density = 0.1 )
-    Q, R, E, rank = qr( M )
+    Q, R, E, rank = qr( M, tolerance = 0 )
     print( abs( Q*R - M*permutation_from_E(E) ).sum() )
