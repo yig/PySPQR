371322619deb6bb433d187b21fecf45d5d90c5da
Juha Jeronen <juha.jeronen@tut.fi>
2017-03-23 14:01:51 +0200

    spqr.py: trailing whitespace cleanup

--- spqr.py
+++ spqr.py
@@ -13,7 +13,7 @@ except ImportError:
     import spqr_gen
     spqr_gen.main()
     print( "=== ...compiled." )
-    
+
     from _spqr import ffi, lib
 
 import scipy.sparse
@@ -25,7 +25,7 @@ Helpful links:
     The primary docs:
     http://cffi.readthedocs.io/en/latest/overview.html
     http://cffi.readthedocs.io/en/latest/using.html
-    
+
     Some helpful examples for #define and NumPy:
     https://kogs-www.informatik.uni-hamburg.de/~seppke/content/teaching/wise1314/20131107_pridoehl-cffi.pdf
 '''
@@ -39,44 +39,44 @@ def deinit():
 
 def scipy2choldmod( scipy_A ):
     scipy_A = scipy_A.tocoo()
-    
+
     nnz = scipy_A.nnz
-    
+
     ## There is a potential performance win if we know A is symmetric and we
     ## can get only the upper or lower triangular elements.
     chol_A = lib.cholmod_l_allocate_triplet( scipy_A.shape[0], scipy_A.shape[1], nnz, 0, lib.CHOLMOD_REAL, cc )
-    
+
     Ai = ffi.cast( "SuiteSparse_long*", chol_A.i )
     Aj = ffi.cast( "SuiteSparse_long*", chol_A.j )
     Avals = ffi.cast( "double*", chol_A.x )
-    
+
     Ai[0:nnz] = scipy_A.row
     Aj[0:nnz] = scipy_A.col
     Avals[0:nnz] = scipy_A.data
-    
+
     chol_A.nnz = nnz
-    
+
     ## Print what cholmod sees as the matrix.
     # lib.cholmod_l_print_triplet( chol_A, "A".encode('utf-8'), cc )
     assert lib.cholmod_l_check_triplet( chol_A, cc ) == 1
-    
+
     ## Convert to a cholmod_sparse matrix.
     result = lib.cholmod_l_triplet_to_sparse( chol_A, nnz, cc )
     ## Free the space used by the cholmod triplet matrix.
     cholmod_free_triplet( chol_A )
-    
+
     return result
 
 def cholmod2scipy( chol_A ):
     ## Convert to a cholmod_triplet matrix.
     chol_A = lib.cholmod_l_sparse_to_triplet( chol_A, cc )
-    
+
     nnz = chol_A.nnz
-    
+
     Ai = ffi.cast( "SuiteSparse_long*", chol_A.i )
     Aj = ffi.cast( "SuiteSparse_long*", chol_A.j )
     Adata = ffi.cast( "double*", chol_A.x )
-    
+
     ## Have to pass through list().
     ## https://bitbucket.org/cffi/cffi/issues/292/cant-copy-data-to-a-numpy-array
     ## http://stackoverflow.com/questions/16276268/how-to-pass-a-numpy-array-into-a-cffi-function-and-how-to-get-one-back-out
@@ -84,7 +84,7 @@ def cholmod2scipy( chol_A ):
     i = numpy.zeros( nnz, dtype = numpy.int64 )
     j = numpy.zeros( nnz, dtype = numpy.int64 )
     data = numpy.zeros( nnz )
-    
+
     i[0:nnz] = list( Ai[0:nnz] )
     j[0:nnz] = list( Aj[0:nnz] )
     data[0:nnz] = list( Adata[0:nnz] )
@@ -95,15 +95,15 @@ def cholmod2scipy( chol_A ):
     i = cffi_asarray.asarray( ffi, Ai, nnz ).copy()
     j = cffi_asarray.asarray( ffi, Aj, nnz ).copy()
     data = cffi_asarray.asarray( ffi, Adata, nnz ).copy()
-    
+
     scipy_A = scipy.sparse.coo_matrix(
         ( data, ( i, j ) ),
         shape = ( chol_A.nrow, chol_A.ncol )
         )
-    
+
     ## Free the space used by the cholmod triplet matrix.
     cholmod_free_triplet( chol_A )
-    
+
     return scipy_A
 
 def cholmod_free_triplet( A ):
@@ -117,7 +117,7 @@ def qr( A, tolerance = None ):
     returns Q, R, E, rank such that:
         Q*R = A*permutation_from_E(E)
     rank is the estimated rank of A.
-    
+
     If optional `tolerance` parameter is negative, it has the following meanings:
         #define SPQR_DEFAULT_TOL ...       /* if tol <= -2, the default tol is used */
         #define SPQR_NO_TOL ...            /* if -2 < tol < 0, then no tol is used */
@@ -128,9 +128,9 @@ def qr( A, tolerance = None ):
     chol_Q = ffi.new("cholmod_sparse**")
     chol_R = ffi.new("cholmod_sparse**")
     chol_E = ffi.new("SuiteSparse_long**")
-    
+
     if tolerance is None: tolerance = lib.SPQR_DEFAULT_TOL
-    
+
     rank = lib.SuiteSparseQR_C_QR(
         ## Input
         lib.SPQR_ORDERING_DEFAULT,
@@ -143,10 +143,10 @@ def qr( A, tolerance = None ):
         chol_E,
         cc
         )
-    
+
     scipy_Q = cholmod2scipy( chol_Q[0] )
     scipy_R = cholmod2scipy( chol_R[0] )
-    
+
     ## If chol_E is null, there was no permutation.
     if chol_E == ffi.NULL:
         E = None
@@ -158,13 +158,13 @@ def qr( A, tolerance = None ):
         # E[0:A.shape[1]] = list( chol_E[0][0:A.shape[1]] )
         ## UPDATE: I can do this without going through list() or making two extra copies.
         E = cffi_asarray.asarray( ffi, chol_E[0], A.shape[1] ).copy()
-    
+
     ## Free cholmod stuff
     lib.cholmod_l_free_sparse( chol_Q, cc )
     lib.cholmod_l_free_sparse( chol_R, cc )
     ## Apparently we don't need to do this. (I get a malloc error.)
     # lib.cholmod_l_free( A.shape[1], ffi.sizeof("SuiteSparse_long"), chol_E, cc )
-    
+
     return scipy_Q, scipy_R, E, rank
 
 def permutation_from_E( E ):
@@ -174,7 +174,7 @@ def permutation_from_E( E ):
 
 if __name__ == '__main__':
     # Q, R, E, rank = qr( scipy.sparse.identity(10) )
-    
+
     M = scipy.sparse.rand( 10, 10, density = 0.1 )
     Q, R, E, rank = qr( M, tolerance = 0 )
     print( abs( Q*R - M*permutation_from_E(E) ).sum() )
