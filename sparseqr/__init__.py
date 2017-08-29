99123cb325e34b2082f0f2be4d84b06ba8fc9e3d
Juha Jeronen <juha.jeronen@jyu.fi>
2017-08-29 12:14:27 +0300

    init: remove outdated comment

--- sparseqr/__init__.py
+++ sparseqr/__init__.py
@@ -16,7 +16,6 @@ See the docstrings of the individual functions for details.
 
 from __future__ import absolute_import
 
-# This is extracted automatically by the top-level setup.py.
 __version__ = '1.0.0'
 
 # add any imports here, if you wish to bring things into the library's top-level namespace when the library is imported.
